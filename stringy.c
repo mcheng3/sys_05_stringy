e5f7d2c923c50d74bd478486bde1645dfee719bc
queeniexiang <wxiang1@stuy.edu>
2017-10-03 16:39:52 -0400

    Fixed strcpy so it works

--- stringy.c
+++ stringy.c
@@ -1,38 +1,44 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int strlen( char * );
-char * strcpy( char *dest, char *source );
-char * strncat( char *dest, char *source, int n);
-int strcmp( char *s1, char *s2 );
-char * strchr( char *s, char c );
+int strlen2( char * );
+char * strcpy2( char *dest, char *source );
+char * strncat2( char *dest, char *source, int n);
+int strcmp2( char *s1, char *s2 );
+char * strchr2( char *s, char c );
 
 int main(){
-	char *s = "Hello!";
-	char *b = "iii";
-	printf("strlen(s): %d\n", strlen(s));
-	printf("strcpy(s, b): [%s]\n", strcpy(s, b));
-	return 0;
+  char s[100] = "Hello!";
+  char b[100] = "iii";
+  char d[100]; 
+  printf("strlen(s): %d\n", strlen2(s));
+  printf("strcpy(s, b): [%s]\n", strcpy2(s, b));
+  return 0;
 }
 
-int strlen(char *s){
-	int i = 0;
-	while(*s){
-		s++;
-		i++;
-	}
-	return i;
+int strlen2(char *s){
+  int i = 0;
+  while(*s){
+    s++;
+    i++;
+  }
+  return i;
 }
 
-char * strcpy( char *dest, char *source){
-	while(*source){
-		printf("%p\n",dest);
-		dest = source;
-		dest++;
-		source++;
-	}
-	//*dest = s;
-	return dest;
-}	
+char * strcpy2( char *dest, char *source){
+  
+  char *src = source;
+  char *des = dest;
+  
+  while (*src) {
+    *des = *src;
+    src++; 
+    des++;
+  }
+
+  *des = '\0'; 
+  return dest;
+}
+	
 			
 
