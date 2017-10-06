e17f4950dfbfd778db26880e7b8c6fd6086bbf29
queeniexiang <wxiang1@stuy.edu>
2017-10-05 21:00:17 -0400

    Added more test cases and fixed format

--- stringy.c
+++ stringy.c
@@ -1,30 +1,68 @@
+/* 
+Queenie Xiang and Michael Cheng
+Systems pd5
+HW05: If you like it then you should've put a string on it
+2017-10-05
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
-int strlen2( char * );
-char * strcpy2( char *dest, char *source );
-char * strncat2( char *dest, char *source, int n);
-int strcmp2( char *s1, char *s2 );
-char * strchr2( char *s, char c );
+int mystrlen( char * );
+char * mystrcpy( char *dest, char *source );
+char * mystrncat( char *dest, char *source, int n);
+int mystrcmp( char *s1, char *s2 );
+char * mystrchr( char *s, char c );
 
 int main(){
   char s[100] = "Hello!";
+  char s1[100] = "??W?"; 
+  char s2[100] = "goodbye"; 
   char b[100] = "iii";
-  char d[100]; 
-	char a[] = "aaca";
-	char c[] = "c";
-  printf("strlen(s): %d\n", strlen2(s));
-  //printf("strcpy(s, b): [%s]\n", strcpy(s, b));
-  //printf("strcpy2(s, b): [%s]\n", strcpy2(s, b));
-  printf("strncat2(s, b, 2): [%s]\n", strncat2(s, b, 2));
-	printf("strcmp2(s, b): %d\n", strcmp2(c,c));
-	printf("strchr2(a, c): %s\n", strchr2(a,'c'));
-	return 0;
+  char d[100] = ""; 
+  char a[] = "aaca";
+  char c[] = "c";
+  char c2[] = "c"; 
+  char e = 'e'; 
 
+    
+  printf("My strlen(%s): %d\n", s, mystrlen(s));
+  printf("Standard strlen(%s): %lu\n\n", s, strlen(s));
+
+  printf("s1: %s\n", s1);
+  printf("s2: %s\n", s2);
+  printf("d: %s\n", d);
+  
+  printf("My strcpy(s1, s2): [%s]\n", mystrcpy(s1, s2));
+  printf("Standard strcpy(d, s2): [%s]\n\n", strcpy(d, s2));
+
+  printf("s: %s\n", s);
+  printf("b: %s\n", b);
+  printf("s1: %s\n", s1); 
+  
+  printf("My strncat(s, b, 2): [%s]\n", mystrncat(s, b, 2));
+  printf("Standard strncat(s1, b, 2): [%s]\n\n",strncat(s1, b, 2));
+
+  printf("a: %s\n", a);
+  printf("c: %s\n", c);
+  printf("c2: %s\n", c2);
+  
+  printf("My strcmp(a, c): %d\n", mystrcmp(a,c));
+  printf("Standard stcmp(a, c): %d\n", strcmp(a, c));
+  printf("My strcmp(c, c): %d\n", mystrcmp(c,c));
+  printf("Standard stcmp(c, c): %d\n\n", strcmp(c, c));
+  
+
+  printf("s: %s\n", s);
+  printf("e: %c\n", e); 
+  printf("My strchr(s, e): %s\n", mystrchr(s,e));
+  printf("Standard strchr(s, e): %s\n\n", strchr(s,e));
+
+  
 }
 
-int strlen2(char *s){
+int mystrlen(char *s){
   int i = 0;
   while(*s){
     s++;
@@ -33,7 +71,7 @@ int strlen2(char *s){
   return i;
 }
 
-char * strcpy2( char *dest, char *source){
+char * mystrcpy( char *dest, char *source){
   
   char *src = source;
   char *des = dest;
@@ -48,35 +86,35 @@ char * strcpy2( char *dest, char *source){
   return dest;
 }
 
-char *strncat2( char * dest, char *source, int n){
-	char *des = dest;
-	char *src = source;
-	while(*des++);
-	*des--;
-	while(n--){
-		printf("des: %s\n", des);
-		*des = *src;
-		src++;
-		des++;
-	}
-	//*des = '\0'; 
-	return dest;
+char * mystrncat( char * dest, char *source, int n){
+  char *des = dest;
+  char *src = source;
+  while(*des++);
+  *des--;
+  while(n--){
+    //printf("des: %s\n", des);
+    *des = *src;
+    src++;
+    des++;
+  }
+  //*des = '\0'; 
+  return dest;
 }
 				
-int strcmp2( char *s1, char *s2){
-	while(s1 == s2 && !s1 && !s2){
-		s1++;
-		s2++;
+int mystrcmp( char *s1, char *s2){
+  while(s1 == s2 && !s1 && !s2){
+    s1++;
+    s2++;
 
-	}
-	return ( s1 - s2);
+  }
+  return ( s1 - s2);
 }
 
-char *strchr2( char *s, char c ){
-	while(*s != c){
-		s++;
-	}
-	return s;			
+char * mystrchr( char *s, char c ){
+  while(*s != c){
+    s++;
+  }
+  return s;			
 }
 
 
